0d96edcd929fe89ac78e6757ccc353c7432336b9
danielfdzperez <daniel.fdz.perez@gmail.com>
2014-05-16 17:56:55 +0200

    menu slide

--- pagina_2_tecnologias_acceso_red/javascript/menu_desplegable.js
+++ pagina_2_tecnologias_acceso_red/javascript/menu_desplegable.js
@@ -5,16 +5,22 @@ function desplegar_menu(id){
     document.getElementById('menu').style.cursor = 'pointer'
 
     clearTimeout(temporizador)
-    if(abrir)
-        abrir.style.visibility = 'hidden'	
+    if(abrir){
+          abrir.style.transitionDuration='1s'
+          abrir.style.height = '0px'	
+    }
     abrir = document.getElementById(id)
-    abrir.style.visibility = 'visible'	
+    /*abrir.style.visibility = 'visible'*/
+    abrir.style.transitionDuration='5s'
+    abrir.style.height = '100%'	
+    
 }
 
 function ocultar_menu(){
-    temporizador = setTimeout(ocultalo, 2000)
+    temporizador = setTimeout(ocultalo, 1000)
     function ocultalo(){
             document.getElementById('menu').style.cursor = 'auto'
-            abrir.style.visibility = 'hidden'
+            abrir.style.transitionDuration='1s'
+            abrir.style.height = '0'
 	}
 }
